b8453852d6ee3a70d865ab9fbaa37f4e9dc7544a
Michael Artner <michael.artner@hotmail.de>
2026-07-11 17:54:15 +0200

    restructured db to use separate tables for every pain layer

--- populate-db.ps1
+++ populate-db.ps1
@@ -3,7 +3,8 @@ param(
     [switch]$Init,
     [switch]$Fill,
     [switch]$Test,
-    [switch]$Reset
+    [switch]$Reset,
+    [switch]$Info
 )
 
 $workspaceRoot = Split-Path -Parent $PSScriptRoot
@@ -35,30 +36,71 @@ Get-Content $configFile | ForEach-Object {
 }
 
 if ($Init) {
+    function Create-Table {
+        param ($TableName, $Additions)
+
+        docker exec -it $containerId psql -U postgres -d pain_db -c "CREATE TABLE $TableName ($COL_ID SERIAL PRIMARY KEY, $COL_AGGR_ID INTEGER REFERENCES $TableName($COL_ID), $COL_VALUE FLOAT NOT NULL, $COL_CATEGORY TEXT NOT NULL $Additions);"
+    }
     Write-Host "Initializing database schema..."
-    docker exec -it $containerId psql -U postgres -d pain_db -c "CREATE TABLE $TABLE_NAME ($COL_ID SERIAL PRIMARY KEY, $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL, $COL_VALUE FLOAT, $COL_DATATYPE TEXT NOT NULL, $COL_PAINORIGIN TEXT);"
+    # create Emotional, Environment, Physical & Socioeconomic Layer Table
+    Create-Table $TN_EMO ", $COL_COUNTRY TEXT NOT NULL, $COL_WORD TEXT NOT NULL"
+    Create-Table $TN_ENV ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+    Create-Table $TN_PHYS ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+    Create-Table $TN_SOCIOECO ", $COL_COUNTRY TEXT NOT NULL"
+    Create-Table $TN_EXPERIMENTAL ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+    Write-Host "Finished initializing!"
 }
 elseif ($Fill) {
-    $csvFile = Join-Path $workspaceRoot "pain\data\dummy\db_data.csv"
-    $dest = "/tmp/db_data.csv"
-    
-    Write-Host "Filling database with dummy data..."
-    docker cp $csvFile "$($containerId):$dest"
-    docker exec $containerId psql -U postgres -d pain_db -c "COPY $TABLE_NAME FROM '$dest' CSV HEADER;"
-    Write-Host "Imported data from $csvFile into $TABLE_NAME table"
+    function Fill-Table {
+        param ($TableName, $csvFile, $destFile)
+
+        Write-Host "Filling $TableName with dummy data..."
+        docker cp $csvFile "$($containerId):$destFile"
+        # copy data from CSV into the table
+        docker exec $containerId psql -U postgres -d pain_db -c "COPY $TableName FROM '$destFile' CSV HEADER;"
+        # reset the sequence for the table's serial ID column
+        docker exec $containerId psql -U postgres -d pain_db -c "SELECT setval(pg_get_serial_sequence('$TableName', '$COL_ID'), COALESCE(MAX($COL_ID), 1)) FROM $TableName;"
+        Write-Host "  Imported data from $csvFile into $TableName table"
+    }
+
+    #$csvRootFolder = Join-Path $workspaceRoot "pain\data\actual"
+    $csvRootFolder = Join-Path $workspaceRoot "pain\data\dummy\data_types"
+    Write-Host "Filling database with data..."
+    Fill-Table $TN_EMO (Join-Path $csvRootFolder "emo.csv") "/tmp/emo.csv"
+    Fill-Table $TN_ENV (Join-Path $csvRootFolder "env.csv") "/tmp/env.csv"
+    Fill-Table $TN_PHYS (Join-Path $csvRootFolder "phys.csv") "/tmp/phys.csv"
+    Fill-Table $TN_SOCIOECO (Join-Path $csvRootFolder "socioeco.csv") "/tmp/socioeco.csv"
+    Fill-Table $TN_EXPERIMENTAL (Join-Path $csvRootFolder "experimental.csv") "/tmp/experimental.csv"
+    Write-Host "Finished importing data!"
 }
 elseif ($Test) {
     Write-Host "Testing database connection and contents..."
-    docker exec -it $containerId psql -U postgres -d pain_db -c "SELECT * FROM $TABLE_NAME LIMIT 7;"
+    docker exec -it $containerId psql -U postgres -d pain_db -c "SELECT * FROM $TN_ENV LIMIT 7;"
 }
 elseif ($Reset) {
+    function Drop-Table {
+        param ($TableName)
+
+        docker exec -it $containerId psql -U postgres -d pain_db -c "DROP TABLE IF EXISTS $TableName;"
+    }
+
     Write-Host "Resetting database..."
-    docker exec -it $containerId psql -U postgres -d pain_db -c "DROP TABLE IF EXISTS $TABLE_NAME;"
+    Drop-Table $TN_EMO
+    Drop-Table $TN_ENV
+    Drop-Table $TN_PHYS
+    Drop-Table $TN_SOCIOECO
+    Drop-Table $TN_EXPERIMENTAL
+}
+elseif ($Info) {
+    Write-Host "Preparing info for database..."
+    Write-Host "TODO"
+    docker exec -it $containerId psql -U postgres -d pain_db #"\dt"
 }
 else {
-    Write-Host "Usage: .\populate-db.ps1 -Init | -Fill | -Test | -Reset"
+    Write-Host "Usage: .\populate-db.ps1 -Init | -Fill | -Test | -Reset | -Info"
     Write-Host "  -Init  : Initialize database schema (run once)"
     Write-Host "  -Fill  : Fill database with dummy data (run after init)"
     Write-Host "  -Test  : Test database connection and contents"
-    Write-Host "  -Reset : Reset database by dropping the $TABLE_NAME table"
+    Write-Host "  -Reset : Reset database by dropping all known tables"
+    Write-Host "  -Info  : Prints some information about the database's tables"
 }
--- populate-db.sh
+++ populate-db.sh
@@ -40,27 +40,59 @@ if [ $# -gt 0 ]; then
     fi
 
     # Check which operation is requested
+    # ############################################################
+    #   INIT
+    # ############################################################
     if [ $1 == "--init" ]; then
+        create_table() {
+            # $1... table name
+            # $2... additional column information
+            echo "  Creating table $1..."
+            docker exec -it "$CONTAINER_ID" psql -U postgres -d pain_db -c "CREATE TABLE $1 ($COL_ID SERIAL PRIMARY KEY, $COL_AGGR_ID INTEGER REFERENCES $TableName($COL_ID), $COL_VALUE FLOAT NOT NULL, $COL_CATEGORY TEXT NOT NULL $2);"
+        }
         echo "Initializing database schema..."
         # initialize database schema based on config file
-        docker exec -it "$CONTAINER_ID" psql -U postgres -d pain_db -c "CREATE TABLE $TABLE_NAME ($COL_ID SERIAL PRIMARY KEY, $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL, $COL_VALUE FLOAT, $COL_DATATYPE TEXT NOT NULL, $COL_PAINORIGIN TEXT);"
+        create_table $TN_EMO ", $COL_COUNTRY TEXT NOT NULL, $COL_WORD TEXT NOT NULL"
+        create_table $TN_ENV ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+        create_table $TN_PHYS ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+        create_table $TN_SOCIOECO ", $COL_COUNTRY TEXT NOT NULL"
+        create_table $TN_EXPERIMENTAL ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
         exit 0
 
+    # ############################################################
+    #   FILL
+    # ############################################################
     elif [ $1 == "--fill" ]; then
-        if [ $# -gt 2 ]; then
-            CSV_FILE="$3"
-        else
-            CSV_FILE="$WORKSPACE_ROOT/pain/data/dummy/db_data.csv"
-        fi
-        DEST="/tmp/db_data.csv"
+        fill_table() {
+            #$1... table name
+            #$2... source path
+            #$3... destination path
+            echo "Filling $1 with dummy data..."
+            docker cp "$2" "$($CONTAINER_ID):$3"
+            # copy data from CSV into the table
+            docker exec $CONTAINER_ID psql -U postgres -d pain_db -c "COPY $1 FROM '$3' CSV HEADER;"
+            # reset the sequence for the table's serial ID column
+            docker exec $CONTAINER_ID psql -U postgres -d pain_db -c "SELECT setval(pg_get_serial_sequence('$1', '$COL_ID'), COALESCE(MAX($COL_ID), 1)) FROM $1;"
+            Write-Host "  Imported data from $2 into $1 table"
+        }
 
+        if [ $# -gt 2 ]; then
+            CSV_ROOT_FOLDER="$3"
+        else
+            CSV_ROOT_FOLDER="$WORKSPACE_ROOT/pain/data/actual"
+        fi
         echo "Filling database with dummy data..."
-        docker cp "$CSV_FILE" "$CONTAINER_ID:$DEST"
-        docker exec "$CONTAINER_ID" psql -U postgres -d pain_db -c "COPY $TABLE_NAME FROM '$DEST' CSV HEADER;"
-        echo "Imported data from $CSV_FILE into $TABLE_NAME table"
+        fill_table $TN_EMO "${CSV_ROOT_FOLDER}/emo.csv") "/tmp/emo.csv"
+        fill_table $TN_ENV "${CSV_ROOT_FOLDER}/env.csv") "/tmp/env.csv"
+        fill_table $TN_PHYS "${CSV_ROOT_FOLDER}/phys.csv") "/tmp/phys.csv"
+        fill_table $TN_SOCIOECO "${CSV_ROOT_FOLDER}/socioeco.csv") "/tmp/socioeco.csv"
+        fill_table $TN_EXPERIMENTAL "${CSV_ROOT_FOLDER}/experimental.csv") "/tmp/experimental.csv"
 
         exit 0
 
+    # ############################################################
+    #   TEST
+    # ############################################################
     elif [ $1 == "--test" ]; then
         echo "Testing database connection and contents..."
         if [ $# -gt 2 ]; then
@@ -71,9 +103,20 @@ if [ $# -gt 0 ]; then
         docker exec -it "$CONTAINER_ID" psql -U postgres -d pain_db -c "SELECT * FROM $TABLE_NAME LIMIT '$LIMIT';"
         exit 0
 
+    # ############################################################
+    #   RESET
+    # ############################################################
     elif [ $1 == "--reset" ]; then
+        drop_table() {
+            #$1... table name
+            docker exec -it "$CONTAINER_ID" psql -U postgres -d pain_db -c "DROP TABLE IF EXISTS $1;"
+        }
         echo "Resetting database..."
-        docker exec -it "$CONTAINER_ID" psql -U postgres -d pain_db -c "DROP TABLE IF EXISTS $TABLE_NAME;"
+        drop_table $TN_EMO
+        drop_table $TN_ENV
+        drop_table $TN_PHYS
+        drop_table $TN_SOCIOECO
+        drop_table $TN_EXPERIMENTAL
         exit 0
     fi
 fi
