f2b9d1ecc009bc834801bfbf1623f983cd98d608
Michael Artner <38492376+7Magic7Mike7@users.noreply.github.com>
2026-08-20 21:38:20 +0200

    Restructure (#3)
    * restructured db to use separate tables for every pain layer
    
    * fixed syntax error in population script
    
    * extended functionality with Clearing & Filling a single named table
    
    * made bash scripts executable
    
    * added  user data tables
    
    * expanded bash scripts to connect to DB & container logs
    
    * improved database typings for user metrics
    
    * turned all db values to lowercase-only & expanded database with types and user tables
    
    - lowercase: while tables & columns are case-insensitive, enum values are not
    
    * fixed bash parsing of .env file
    
    * updated default path for --fill
    
    * pop bash script can now also clear and fill individual columns

--- populate-db.ps1
+++ populate-db.ps1
@@ -2,8 +2,11 @@
 param(
     [switch]$Init,
     [switch]$Fill,
+    [string]$FillTarget,
     [switch]$Test,
-    [switch]$Reset
+    [switch]$Reset,
+    [switch]$Connect,
+    [string]$Clear
 )
 
 $workspaceRoot = Split-Path -Parent $PSScriptRoot
@@ -35,30 +38,120 @@ Get-Content $configFile | ForEach-Object {
 }
 
 if ($Init) {
+    function Create-Table {
+        param ($TableName, $Additions)
+
+        docker exec -it $containerId psql -U postgres -d pain_db -c "CREATE TABLE $TableName ($COL_ID SERIAL PRIMARY KEY $Additions);"
+    }
+    function Create-Data-Table {
+        param ($TableName, $Additions)
+
+        Create-Table $TableName ", $COL_AGGR_ID INTEGER REFERENCES $TableName($COL_ID), $COL_VALUE FLOAT NOT NULL, $COL_CATEGORY TEXT NOT NULL $Additions"
+    }
+    function Create-Metrics-Table {
+        param ($TableName, $Additions)
+
+        Create-Table $TableName ", $COL_DT $TYPE_DT NOT NULL, $COL_USER_ID INTEGER REFERENCES $TNM_USERS($COL_ID) $Additions"
+    }
+    function Create-Enum {
+        param ($EnumName, $EnumValues)
+
+        docker exec -it $containerId psql -U postgres -d pain_db -c "CREATE TYPE ${EnumName} AS ENUM (${EnumValues});"
+    }
     Write-Host "Initializing database schema..."
-    docker exec -it $containerId psql -U postgres -d pain_db -c "CREATE TABLE $TABLE_NAME ($COL_ID SERIAL PRIMARY KEY, $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL, $COL_VALUE FLOAT, $COL_DATATYPE TEXT NOT NULL, $COL_PAINORIGIN TEXT);"
+    # create Emotional, Environment, Physical & Socioeconomic Layer Table
+    Create-Data-Table $TN_EMO ", $COL_COUNTRY TEXT NOT NULL, $COL_WORD TEXT NOT NULL"
+    Create-Data-Table $TN_ENV ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+    Create-Data-Table $TN_PHYS ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+    Create-Data-Table $TN_SOCIOECO ", $COL_COUNTRY TEXT NOT NULL"
+    Create-Data-Table $TN_EXPERIMENTAL ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+    # create enums for metrics tables
+    Create-Enum ${TYPE_TOGGLE_KIND} ${VALUES_TYPE_TOGGLE_KIND}
+    Create-Enum ${TYPE_TOGGLE_ELEM} "'${TN_EMO}', '${TN_ENV}', '${TN_PHYS}', '${TN_SOCIOECO}', '${TN_EXPERIMENTAL}', ${VALUES_TYPE_TOGGLE_ELEM_PAIN}, ${VALUES_TYPE_TOGGLE_ELEM_TEMPORALITY}, ${VALUES_TYPE_TOGGLE_ELEM_RELATIONS}"
+    Create-Enum ${TYPE_VIS_MODE} ${VALUES_TYPE_VIS_MODE}
+    # create metrics tables
+    Create-Table $TNM_USERS ", $COL_DT $TYPE_DT NOT NULL, $COL_USER_ID TEXT NOT NULL"
+    Create-Metrics-Table $TNM_USER_COORDINATES ", $COL_LAT FLOAT NOT NULL, $COL_LNG FLOAT NOT NULL"
+    Create-Metrics-Table $TNM_TOGGLE ", $COL_KIND ${TYPE_TOGGLE_KIND} NOT NULL, $COL_ELEM ${TYPE_TOGGLE_ELEM} NOT NULL, $COL_ENABLED BOOL NOT NULL"
+    Create-Metrics-Table $TNM_STEP ", $COL_STEP SMALLINT NOT NULL CHECK ($COL_STEP BETWEEN 0 AND $TYPE_STEP_MAX)"
+    Create-Metrics-Table $TNM_VIS ", $COL_VIS_MODE ${TYPE_VIS_MODE} NOT NULL"
+
+    Write-Host "Finished initializing!"
 }
 elseif ($Fill) {
-    $csvFile = Join-Path $workspaceRoot "pain\data\dummy\db_data.csv"
-    $dest = "/tmp/db_data.csv"
-    
-    Write-Host "Filling database with dummy data..."
-    docker cp $csvFile "$($containerId):$dest"
-    docker exec $containerId psql -U postgres -d pain_db -c "COPY $TABLE_NAME FROM '$dest' CSV HEADER;"
-    Write-Host "Imported data from $csvFile into $TABLE_NAME table"
+    function Fill-Table {
+        param ($TableName, $csvFile, $destFile)
+
+        Write-Host "Filling $TableName with dummy data..."
+        docker cp $csvFile "$($containerId):$destFile"
+        # copy data from CSV into the table
+        docker exec $containerId psql -U postgres -d pain_db -c "COPY $TableName FROM '$destFile' CSV HEADER;"
+        # reset the sequence for the table's serial ID column
+        docker exec $containerId psql -U postgres -d pain_db -c "SELECT setval(pg_get_serial_sequence('$TableName', '$COL_ID'), COALESCE(MAX($COL_ID), 1)) FROM $TableName;"
+        Write-Host "  Imported data from $csvFile into $TableName table"
+    }
+
+    #$csvRootFolder = Join-Path $workspaceRoot "pain\data\actual"
+    $csvRootFolder = Join-Path $workspaceRoot "pain\data\dummy\data_types"
+    Write-Host "Filling database with data..."
+    if ($FillTarget) {
+        Fill-Table "${FillTarget}pain" (Join-Path $csvRootFolder "$FillTarget.csv") "/tmp/$FillTarget.csv"
+    }
+    else {
+        Fill-Table $TN_EMO (Join-Path $csvRootFolder "emo.csv") "/tmp/emo.csv"
+        Fill-Table $TN_ENV (Join-Path $csvRootFolder "env.csv") "/tmp/env.csv"
+        Fill-Table $TN_PHYS (Join-Path $csvRootFolder "phys.csv") "/tmp/phys.csv"
+        Fill-Table $TN_SOCIOECO (Join-Path $csvRootFolder "socioeco.csv") "/tmp/socioeco.csv"
+        Fill-Table $TN_EXPERIMENTAL (Join-Path $csvRootFolder "experimental.csv") "/tmp/experimental.csv"
+    }
+    Write-Host "Finished importing data!"
 }
 elseif ($Test) {
     Write-Host "Testing database connection and contents..."
-    docker exec -it $containerId psql -U postgres -d pain_db -c "SELECT * FROM $TABLE_NAME LIMIT 7;"
+    docker exec -it $containerId psql -U postgres -d pain_db -c "SELECT * FROM $TN_ENV LIMIT 7;"
 }
 elseif ($Reset) {
+    function Drop {
+        param ($Type, $TableName)
+
+        docker exec -it $containerId psql -U postgres -d pain_db -c "DROP $Type IF EXISTS $TableName;"
+        Write-Host " -dropped $Type $TableName"
+    }
+
     Write-Host "Resetting database..."
-    docker exec -it $containerId psql -U postgres -d pain_db -c "DROP TABLE IF EXISTS $TABLE_NAME;"
+    # drop data tables
+    Drop "TABLE" $TN_EMO
+    Drop "TABLE" $TN_ENV
+    Drop "TABLE" $TN_PHYS
+    Drop "TABLE" $TN_SOCIOECO
+    Drop "TABLE" $TN_EXPERIMENTAL
+    # drop user tables
+    Drop "TABLE" $TNM_TOGGLE
+    Drop "TABLE" $TNM_STEP
+    Drop "TABLE" $TNM_VIS
+    Drop "TABLE" $TNM_USER_COORDINATES
+    Drop "TABLE" $TNM_USERS
+    # drop types
+    Drop "TYPE" ${TYPE_TOGGLE_KIND}
+    Drop "TYPE" ${TYPE_TOGGLE_ELEM}
+    Drop "TYPE" ${TYPE_VIS_MODE}
+    Write-Host "Done dropping!"
+}
+elseif ($Connect) {
+    Write-Host "Connecting to database..."
+    docker exec -it $containerId psql -U postgres -d pain_db #"\dt"
+}
+elseif ($Clear) {
+    Write-Host "Clearing table ${Clear}pain"
+    docker exec -it $containerId psql -U postgres -d pain_db -c "TRUNCATE ${Clear}pain"
 }
 else {
-    Write-Host "Usage: .\populate-db.ps1 -Init | -Fill | -Test | -Reset"
+    Write-Host "Usage: .\populate-db.ps1 -Init | -Fill | -Test | -Reset | -Info"
     Write-Host "  -Init  : Initialize database schema (run once)"
     Write-Host "  -Fill  : Fill database with dummy data (run after init)"
+    Write-Host "        -FillTarget: additional argument to determine which table to fill"
     Write-Host "  -Test  : Test database connection and contents"
-    Write-Host "  -Reset : Reset database by dropping the $TABLE_NAME table"
+    Write-Host "  -Reset : Reset database by dropping all known tables"
+    Write-Host "  -Connect  : Connect to the Database"
+    Write-Host "  -Clear : Clears all rows from a specified table"
 }
